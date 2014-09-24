0ce42ad7ec53747c8021075813c669aa51f716dc
Patrick Forget <patforg@geekpad.ca>
2014-09-24 15:53:17 -0400

    input with a fixed value

---
+++ src/Informant/Input/FixedValueInput.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * class for user entered input
+ *
+ * @since  2014-05-08
+ * @author Patrick Forget <patforg@geekpad.ca>
+ */
+
+namespace Informant\Input;
+
+/**
+ * class for user entered input
+ *
+ * @since  2014-05-08
+ * @author Patrick Forget <patforg@geekpad.ca>
+ */
+class FixedValueInput extends BaseInput
+{
+
+} //  FixedValueInput class
